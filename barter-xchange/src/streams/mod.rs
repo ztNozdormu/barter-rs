c48b93221928fe1f297f5fed79131483b89f0cd3
www1 <w1999wtw3537@sina.com>
2025-01-26 11:49:52 +0800

    xchange remove streams codes

--- barter-xchange/src/streams/mod.rs
+++
@@ -1,6 +0,0 @@
-
-/// Defines the [`StreamBuilder`] and [`MultiStreamBuilder`] APIs for ergonomically initialising
-/// [`MarketStream`](super::MarketStream) [`Streams`].
-pub mod builder;
-
-pub mod consumer;
