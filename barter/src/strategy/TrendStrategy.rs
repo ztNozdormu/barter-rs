2042abb011409ad2ff8b48fba043185ac1d2e849
www1 <w1999wtw3537@sina.com>
2025-01-26 14:34:03 +0800

    add new examples new TrendStrategy

---
+++ barter/src/strategy/TrendStrategy.rs
@@ -0,0 +1,122 @@
+use std::marker::PhantomData;
+use serde::{Deserialize, Serialize};
+use barter_data::event::MarketEvent;
+use barter_execution::AccountEvent;
+use barter_execution::order::id::StrategyId;
+use barter_execution::order::{Order, RequestCancel, RequestOpen};
+use barter_instrument::asset::AssetIndex;
+use barter_instrument::exchange::{ExchangeId, ExchangeIndex};
+use barter_instrument::instrument::InstrumentIndex;
+use crate::engine::{Engine, Processor};
+use crate::engine::state::EngineState;
+use crate::engine::state::instrument::filter::InstrumentFilter;
+use crate::engine::state::instrument::market_data::MarketDataState;
+use crate::strategy::algo::AlgoStrategy;
+use crate::strategy::close_positions::{close_open_positions_with_market_orders, ClosePositionsStrategy};
+use crate::strategy::on_disconnect::OnDisconnectStrategy;
+use crate::strategy::on_trading_disabled::OnTradingDisabled;
+
+/// Naive implementation of all strategy interfaces.
+///
+/// *THIS IS FOR DEMONSTRATION PURPOSES ONLY, NEVER USE FOR REAL TRADING OR IN PRODUCTION*.
+///
+/// This strategy:
+/// - Generates no algorithmic orders (AlgoStrategy).
+/// - Closes positions via the naive [`close_open_positions_with_market_orders`] logic (ClosePositionsStrategy).
+/// - Does nothing when an exchange disconnects (OnDisconnectStrategy).
+/// - Does nothing when trading state is set to disabled (OnDisconnectStrategy).
+#[derive(Debug, Clone)]
+pub struct TrendStrategy<State> {
+    pub id: StrategyId,
+    phantom: PhantomData<State>,
+}
+
+impl<State> Default for TrendStrategy<State> {
+    fn default() -> Self {
+        Self {
+            id: StrategyId::new("default"),
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<State, ExchangeKey, InstrumentKey> AlgoStrategy<ExchangeKey, InstrumentKey>
+for TrendStrategy<State>
+{
+    type State = State;
+
+    fn generate_algo_orders(
+        &self,
+        _: &Self::State,
+    ) -> (
+        impl IntoIterator<Item = Order<ExchangeKey, InstrumentKey, RequestCancel>>,
+        impl IntoIterator<Item = Order<ExchangeKey, InstrumentKey, RequestOpen>>,
+    ) {
+        (std::iter::empty(), std::iter::empty())
+    }
+}
+
+impl<MarketState, StrategyState, RiskState> ClosePositionsStrategy
+for TrendStrategy<EngineState<MarketState, StrategyState, RiskState>>
+where
+    MarketState: MarketDataState,
+{
+    type State = EngineState<MarketState, StrategyState, RiskState>;
+
+    fn close_positions_requests<'a>(
+        &'a self,
+        state: &'a Self::State,
+        filter: &'a InstrumentFilter,
+    ) -> (
+        impl IntoIterator<Item = Order<ExchangeIndex, InstrumentIndex, RequestCancel>> + 'a,
+        impl IntoIterator<Item = Order<ExchangeIndex, InstrumentIndex, RequestOpen>> + 'a,
+    )
+    where
+        ExchangeIndex: 'a,
+        AssetIndex: 'a,
+        InstrumentIndex: 'a,
+    {
+        close_open_positions_with_market_orders(&self.id, state, filter)
+    }
+}
+
+impl<Clock, State, ExecutionTxs, Risk> OnDisconnectStrategy<Clock, State, ExecutionTxs, Risk>
+for TrendStrategy<State>
+{
+    type OnDisconnect = ();
+
+    fn on_disconnect(
+        _: &mut Engine<Clock, State, ExecutionTxs, Self, Risk>,
+        _: ExchangeId,
+    ) -> Self::OnDisconnect {
+    }
+}
+
+impl<Clock, State, ExecutionTxs, Risk> OnTradingDisabled<Clock, State, ExecutionTxs, Risk>
+for TrendStrategy<State>
+{
+    type OnTradingDisabled = ();
+
+    fn on_trading_disabled(
+        _: &mut Engine<Clock, State, ExecutionTxs, Self, Risk>,
+    ) -> Self::OnTradingDisabled {
+    }
+}
+
+/// Empty strategy state that can be used for strategies that require no specific global state.
+#[derive(
+    Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default, Deserialize, Serialize,
+)]
+pub struct TrendStrategyState;
+
+impl<ExchangeKey, AssetKey, InstrumentKey>
+Processor<&AccountEvent<ExchangeKey, AssetKey, InstrumentKey>> for TrendStrategyState
+{
+    type Audit = ();
+    fn process(&mut self, _: &AccountEvent<ExchangeKey, AssetKey, InstrumentKey>) -> Self::Audit {}
+}
+
+impl<InstrumentKey, Kind> Processor<&MarketEvent<InstrumentKey, Kind>> for TrendStrategyState {
+    type Audit = ();
+    fn process(&mut self, _: &MarketEvent<InstrumentKey, Kind>) -> Self::Audit {}
+}
