e6db9b9965cd37332df1134c3c494facfe3fa480
ztNozdormu <w1999wtw3537@sina.com>
2025-01-29 17:52:18 +0800

    klines update to tuple

--- barter-data/examples/kline_stream_exchange.rs
+++ barter-data/examples/kline_stream_exchange.rs
@@ -23,7 +23,7 @@ async fn main() {
     // '--> each call to StreamBuilder::subscribe() creates a separate WebSocket connection
     let streams = Streams::<KLines>::builder()
         .subscribe([
-            (BinanceFuturesUsd::default(), "btc", "usdt", MarketDataInstrumentKind::Perpetual, KLines{iv:"1m"}),
+            (BinanceFuturesUsd::default(), "btc", "usdt", MarketDataInstrumentKind::Perpetual, KLines(1)),
             // (BinanceFuturesUsd::default(), "eth", "usdt", MarketDataInstrumentKind::Perpetual, Tickers),
         ])
         .init()
